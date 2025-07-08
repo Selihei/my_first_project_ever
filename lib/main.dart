c768a3270ee471f5fc3063236480f08e94d3f809
Selihei <selina.heinrichlp@gmail.com>
2025-07-08 15:34:24 +0200

    noch mehr

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,20 @@ class MainApp extends StatelessWidget {
             mainAxisSize: MainAxisSize.max,
             children: [
               Text("Hello World"),
-              Text("Child 2")
+              Text("Child 2"),
+              Divider(height: 100, thickness:20, color: Color(0xFFFFFF33)),
+              Container(
+                color: Colors.black,
+                child: Text("Ich bin ein Container lol", style: TextStyle(color:Colors.white),)
+              ),
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Maximaler ", style: TextStyle(color: Color(0xFFFF073A), fontWeight: FontWeight.bold)),
+                  Text("Augenkrebs", style:  TextStyle(color: Color(0xFF1F51FF), fontWeight: FontWeight.bold))
+                ],
+              )
             ],)
           ),
         )
