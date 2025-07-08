c1f9128864dc6cac356a7105dc6905526b6d156c
Selihei <selina.heinrichlp@gmail.com>
2025-07-08 16:17:59 +0200

    format bessere ansicht

--- lib/main.dart
+++ lib/main.dart
@@ -13,30 +13,51 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color(0xFF1FFFFF),
-        appBar: AppBar(backgroundColor: Color(0xFFFF10F0), centerTitle: true, title:  Text("Test")),
-        body:  Center(
+        appBar: AppBar(
+          backgroundColor: Color(0xFFFF10F0),
+          centerTitle: true,
+          title: Text("Test"),
+        ),
+        body: Center(
           child: Column(
             mainAxisSize: MainAxisSize.max,
             children: [
               Text("Hello World"),
               Text("Child 2"),
-              Divider(height: 100, thickness:20, color: Color(0xFFFFFF33)),
+              Divider(height: 100, thickness: 20, color: Color(0xFFFFFF33)),
               Container(
                 color: Colors.black,
-                child: Text("Ich bin ein Container lol", style: TextStyle(color:Colors.white),)
+                child: Text(
+                  "Ich bin ein Container lol",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
               SizedBox(height: 50),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Maximaler ", style: TextStyle(color: Color(0xFFFF073A), fontWeight: FontWeight.bold)),
-                  Text("Augenkrebs", style:  TextStyle(backgroundColor: 	Color(0xFF00FF00),color: Color(0xFF1F51FF), fontWeight: FontWeight.bold))
+                  Text(
+                    "Maximaler ",
+                    style: TextStyle(
+                      color: Color(0xFFFF073A),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Augenkrebs",
+                    style: TextStyle(
+                      backgroundColor: Color(0xFF00FF00),
+                      color: Color(0xFF1F51FF),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ],
-              )
-            ],)
+              ),
+            ],
           ),
-        )
-      );
+        ),
+      ),
+    );
     // Column ist Spalte, Row ist Reihe
   }
 }
